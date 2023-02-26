308c3176c068491984b14257cfa051f10c24063a
smatsushima1 <smatsushima1@gmail.com>
2023-02-26 16:22:07 -0600

    only do get

--- src/p_functions.py
+++ src/p_functions.py
@@ -77,18 +77,18 @@ async def main():
     response = await request(f"{baseurl}/posts/2", method="GET", headers=headers)
     print(f"GET request=> status:{response.status}, json:{await response.json()}")
 
-    # POST
-    body = json.dumps({"title": "test_title", "body": "test body", "userId": 1})
-    new_post = await request(f"{baseurl}/posts", body=body, method="POST", headers=headers)
-    print(f"POST request=> status:{new_post.status}, json:{await new_post.json()}")
+    # # POST
+    # body = json.dumps({"title": "test_title", "body": "test body", "userId": 1})
+    # new_post = await request(f"{baseurl}/posts", body=body, method="POST", headers=headers)
+    # print(f"POST request=> status:{new_post.status}, json:{await new_post.json()}")
 
-    # PUT
-    body = json.dumps({"id": 1, "title": "test_title", "body": "test body", "userId": 2})
-    new_post = await request(f"{baseurl}/posts/1", body=body, method="PUT", headers=headers)
-    print(f"PUT request=> status:{new_post.status}, json:{await new_post.json()}")
+    # # PUT
+    # body = json.dumps({"id": 1, "title": "test_title", "body": "test body", "userId": 2})
+    # new_post = await request(f"{baseurl}/posts/1", body=body, method="PUT", headers=headers)
+    # print(f"PUT request=> status:{new_post.status}, json:{await new_post.json()}")
 
-    # DELETE
-    new_post = await request(f"{baseurl}/posts/1", method="DELETE", headers=headers)
-    print(f"DELETE request=> status:{new_post.status}, json:{await new_post.json()}")
+    # # DELETE
+    # new_post = await request(f"{baseurl}/posts/1", method="DELETE", headers=headers)
+    # print(f"DELETE request=> status:{new_post.status}, json:{await new_post.json()}")
 
 # asyncio.ensure_future(main())
