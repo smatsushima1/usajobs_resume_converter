4907d3b7ecd981906981aed6fcc6fa873727741f
smatsushima1 <smatsushima1@gmail.com>
2023-02-26 16:07:05 -0600

    try asyncio

--- src/p_functions.py
+++ src/p_functions.py
@@ -36,3 +36,59 @@ def pull_posting():
     Element('dt').write(dtitle)
     Element('ot').write(otitle)
     Element('ad').write(all_duties)
+
+
+from pyodide.http import pyfetch, FetchResponse
+from typing import Optional, Any
+
+async def request(url: str, method: str = "GET", body: Optional[str] = None,
+                  headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
+    """
+    Async request function. Pass in Method and make sure to await!
+    Parameters:
+        url: str = URL to make request to
+        method: str = {"GET", "POST", "PUT", "DELETE"} from `JavaScript` global fetch())
+        body: str = body as json string. Example, body=json.dumps(my_dict)
+        headers: dict[str, str] = header as dict, will be converted to string...
+            Example, headers=json.dumps({"Content-Type": "application/json"})
+        fetch_kwargs: Any = any other keyword arguments to pass to `pyfetch` (will be passed to `fetch`)
+    Return:
+        response: pyodide.http.FetchResponse = use with .status or await.json(), etc.
+    """
+    kwargs = {"method": method, "mode": "cors"}  # CORS: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+    if body and method not in ["GET", "HEAD"]:
+        kwargs["body"] = body
+    if headers:
+        kwargs["headers"] = headers
+    kwargs.update(fetch_kwargs)
+
+    response = await pyfetch(url, **kwargs)
+    return response
+
+
+import asyncio
+import json
+
+async def main():
+    baseurl = "https://www.usajobs.gov/job/707979000"
+
+    # GET
+    headers = {"Content-type": "application/json"}
+    response = await request(f"{baseurl}/posts/2", method="GET", headers=headers)
+    print(f"GET request=> status:{response.status}, json:{await response.json()}")
+
+    # POST
+    body = json.dumps({"title": "test_title", "body": "test body", "userId": 1})
+    new_post = await request(f"{baseurl}/posts", body=body, method="POST", headers=headers)
+    print(f"POST request=> status:{new_post.status}, json:{await new_post.json()}")
+
+    # PUT
+    body = json.dumps({"id": 1, "title": "test_title", "body": "test body", "userId": 2})
+    new_post = await request(f"{baseurl}/posts/1", body=body, method="PUT", headers=headers)
+    print(f"PUT request=> status:{new_post.status}, json:{await new_post.json()}")
+
+    # DELETE
+    new_post = await request(f"{baseurl}/posts/1", method="DELETE", headers=headers)
+    print(f"DELETE request=> status:{new_post.status}, json:{await new_post.json()}")
+
+# asyncio.ensure_future(main())
