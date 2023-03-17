fc501cc3b46b7f21cf1df734db434468678dc53c
smatsushima1 <smatsushima1@gmail.com>
2023-03-17 12:07:16 -0500

    update python to function like jscript

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-import os, time
+from bs4 import BeautifulSoup as bsp
+import os, time, spacy, requests
 
 
 # Start timer for functinos
@@ -27,20 +28,20 @@ def end_function(start_time):
 def convert_all():
     start_time = start_function('convert_all')    
     # First move all files in 'converted_resumes' to 'resumes'
-    ld = os.listdir('converted_resumes')
+    ld = os.listdir('resumes_converted')
     if len(ld) > 0:
         while True:
-            answer = input('Move files in the "converted_resumes" folder before proceeding? (y/n): ')
+            answer = input('Move files in the "resumes_converted" folder before proceeding? (y/n): ')
             if answer.lower().strip() not in ('y', 'n'):
                 print("You must type either 'y' or 'n'.")
             else:
                 break
         if answer.lower().strip() == 'y':
             for i in ld:
-                os.rename('converted_resumes/' + i, 'resumes/' + i)
+                os.rename('resumes_converted/' + i, 'resumes/' + i)
                 print('Moved: ' + i)
     print('\nProceeding to file conversions...\n')
-    for x, i in enumerate(os.scandir('convert_resumes')):
+    for x, i in enumerate(os.scandir('resumes_working')):
         fname = i.name
         if i.name.endswith('.txt'):
             print('Converting: ' + fname)
@@ -53,37 +54,50 @@ def convert_all():
 # Check file
 def convert_file(file_name, iteration):
     # Read file
-    with open('convert_resumes/' + file_name, encoding = 'utf8') as mr:
+    with open('resumes_working/' + file_name, encoding = 'utf8') as mr:
         lines = mr.readlines()
         mr.close()
-    # First find where resume starts
-    for x, i in enumerate(lines):
-        if '# RESUME' in i:
-            resume_start = x
     # Start appending resume after the line it starts
     file_text = ''
     for x, i in enumerate(lines):
-        if x > resume_start:
-            # First iteration does away with '- ' since it won't need a ' '
-            if x == resume_start + 1:
-                file_text += i.rstrip()[2:]
-                continue
-            # Remove line breaks, start only after -
-            file_text += i.rstrip()[1:]
+        # Continue if empty line
+        if i == '\n':
+            continue
+        # Start at first character, remove line break, add period and space
+        file_text += i[int(i.find(next(filter(str.isalpha, i)))):].rstrip() + '. '
+        # Remove extra period
+        file_text = file_text.replace('..', '.')
     # Pull data from file name
     resume_info = file_name.replace('.txt', '').split('_')
-    header_text = '%s\nLENGTH: %s' % (resume_info[1].upper(), len(file_text))
+    header_text = '%s\nLENGTH: %s' % (resume_info[1].upper(), len(file_text.rstrip()))
     print(header_text + '\n')
     # Append text to final converted resumes
     if iteration == 0:
-        with open('converted_resumes/' + resume_info[2] + '_' + datetime.now().strftime('%Y%m%d') + '.txt', 'w', encoding = 'utf8') as wf:
-            wf.writelines('%s\n%s\n\n' % (header_text, file_text))
+        with open('resumes_converted/' + resume_info[2] + '_' + datetime.now().strftime('%Y%m%d') + '.txt', 'w', encoding = 'utf8') as wf:
+            wf.writelines('%s\n%s\n\n' % (header_text, file_text.rstrip()))
     else:
-        with open('converted_resumes/' + resume_info[2] + '_' + datetime.now().strftime('%Y%m%d') + '.txt', 'a', encoding = 'utf8') as wf:
-            wf.writelines('%s\n%s\n\n' % (header_text, file_text))
+        with open('resumes_converted/' + resume_info[2] + '_' + datetime.now().strftime('%Y%m%d') + '.txt', 'a', encoding = 'utf8') as wf:
+            wf.writelines('%s\n%s\n\n' % (header_text, file_text.rstrip()))
 
 
-# convert_all()
+# Find keywords
+def keyword_search():
+    spcy = spacy.load("en_core_web_sm")
+
+    # taking input
+    text = "Writing"
+      
+    # returns a document of object
+    doc = spcy(text)
+      
+    # checking if it is a noun or not
+    if(doc[0].tag_ == 'NNP'):
+        print(text, " is a noun.")
+    else:
+        print(text, " is not a noun.")
+
+
+keyword_search()
 
 
 
